9f516030ecc9672739f832609003ba37bfa3e613
bell-wu <wu.bell0322@gmail.com>
2019-10-02 00:04:24 -0700

    removed Place class

--- game.py
+++ game.py
@@ -23,11 +23,11 @@ class Board(object):
 			self.all_camels = [c for c in board.all_camels]
 			self.moved_camels = [c for c in board.moved_camels]
 		else:
-			self.state = [Place([]) for x in range(NUM_TILES)]
+			self.state = [[[], 0] for x in range(NUM_TILES)]
 
 			if starting != None:
 				for p in starting.keys():
-					self.state[p].add_camels(starting[p])
+					self.state[p][0] = starting[p]
 					self.all_camels += starting[p]
 
 	def move_camel(self, camel_id, num_spaces, position=None):
@@ -55,45 +55,75 @@ class Board(object):
 
 		if position == None:
 			position = self.find_camel_position(camel_id)
-		removed_camels = self.state[position].remove_camel(camel_id)
+
+		removed_camels = self.remove_camel(camel_id, position)
 		
 		# check for powerup
 		end_position = position + num_spaces
 		landed_on_powerup = False
 
-		if end_position < NUM_TILES and self.state[end_position].powerup != 0:
+		if end_position < NUM_TILES and self.state[end_position][1] != 0:
 			landed_on_powerup = True
 
 		# check for winner
-		if end_position >= NUM_TILES - 1 or (end_position == NUM_TILES - 2 and self.state[end_position].powerup == 1):
+		if end_position >= NUM_TILES - 1 or (end_position == NUM_TILES - 2 and self.state[end_position][1] == 1):
 			end_position = NUM_TILES - 1
-			self.state[end_position].add_camels(removed_camels)
+			self.add_camels(removed_camels, end_position)
 
 			# WINNER = self.board.winning_camel()
 			return landed_on_powerup
 
-		powerup = self.state[end_position].powerup
+		powerup = self.state[end_position][1]
 		end_position += powerup
 
 		if powerup == -1:
-			self.state[end_position].add_camels(removed_camels, True)
+			self.add_camels(removed_camels, end_position, True)
 		else:
-			self.state[end_position].add_camels(removed_camels)
+			self.add_camels(removed_camels, end_position)
 		
 		self.moved_camels += [camel_id]
 		return landed_on_powerup
+
+	def add_camels(self, camels_to_add, position, under=False):
+		"""Add camels to this Place
+
+		camels_to_add -- list of camels to be added
+		position -- where to add the camels
+		under -- if True, place camels under instead
+		"""
+		camels = self.state[position][0]
+		for c in camels_to_add:
+			if c in camels:
+				assert False, "Camel already here!"
+
+		if under:
+			self.state[position][0] = camels_to_add + self.state[position][0]
+		else:
+			camels += camels_to_add
+
+	def remove_camel(self, camel_id, position):
+		"""Remove camel with camel_id from this Place, as well as all
+		camels that are on top of this camel.
+
+		return -- list of camels (in same order) that were removed"""
+		index = -1
+
+		for i in range(len(self.state[position][0])):
+			if self.state[position][0][i] == camel_id:
+				index = i
+				break
+
+		r = self.state[position][0][index:]
+		self.state[position][0] = self.state[position][0][:index]
+		return r
 
 	def find_camel_position(self, camel_id):
 		"""Find the position of camel_id
 
 		return -- position (int) of camel_id"""
-		position = -1
 		for i in range(len(self.state)):
-			p = self.state[i]
-			if p.has_camel_here(camel_id):
-				position = i
-				break
-		return position
+			if camel_id in self.state[i][0]:
+				return i
 
 	def add_powerup(self, position, powerup):
 		"""Add powerup (-1 or 1) at position
@@ -104,10 +134,16 @@ class Board(object):
 		>>> b
 		[[1, 2, 3][0], [4][0], [5][0], [][0], [][-1], [][0]
 		"""
-		if position >= NUM_TILES - 1:
-			assert False, "Cannot place powerup at last position"
+		if position >= NUM_TILES - 1 or position <= 0:
+			assert False, "Cannot place powerup here: Invalid Position."
 
-		self.state[position].place_powerup(powerup)
+		if self.state[position][0] != []:
+			assert False, "Cannot place powerup here: Camels already here."
+
+		if not (powerup == 1 or powerup == -1):
+			assert False, "Powerup must be -1 or 1!"
+
+		self.state[position][1] = powerup
 
 	def remove_powerup(self, position):
 		"""Remove powerup (reset to 0) at position
@@ -118,7 +154,7 @@ class Board(object):
 		>>> b
 		[[1, 2, 3][0], [4][0], [5][0], [][0], [][0], [][0]
 		"""
-		self.state[position].remove_powerup()
+		self.state[position][1] = 0
 
 	def new_round(self):
 		"""Defines a new round."""
@@ -126,141 +162,46 @@ class Board(object):
 		self.moved_camels = []
 
 		# remove all powerups
-		for p in self.state:
-			p.remove_powerup()
+		for place in self.state:
+			place[1] = 0
 
 	def losing_camel(self):
 		"""Returns id of the camel in last place."""
-		for p in self.state:
-			if p.has_camels():
-				return p.losing_camel()
+		for place in self.state:
+			if place[0] != []:
+				return place[0][0]
 
 	def winning_camel(self):
 		"""Returns id of the camel in first place."""
 		for i in range(len(self.state) - 1, 0, -1):
-			p = self.state[i]
-			if p.has_camels():
-				return p.winning_camel()
+			camels = self.state[i][0]
+			if camels != []:
+				return camels[-1]
 
 	def second_place_camel(self):
 		"""Returns id of the camel in second place."""
 		found_first = False
 		for i in range(len(self.state) - 1, 0, -1):
-			p = self.state[i]
-			if p.has_camels():
-				if len(p.camels) == 1:
+			camels = self.state[i][0]
+			if len(camels) > 0:
+				if found_first:
+					return camels[-1]
+				if len(camels) == 1:
 					found_first = True
 				else:
-					return p.camels[len(p.camels) - 2]
-			if found_first:
-				return p.winning_camel()
-		return 0
+					return camels[len(camels) - 2]
 
-	def add_camel(self, camel_id, position):
-		"""Adds a camel at position."""
-		self.state[position].add_camels([camel_id])
+	# def add_camel(self, camel_id, position):
+	# 	"""Adds a camel at position."""
+	# 	# self.state[position].add_camels([camel_id])
+	# 	self.state[position][0] += [camel_id]
 
 	def fast_copy_state(self):
 		"""Returns a copy of self.state, and does it FAST."""
-		return [p.fast_copy() for p in self.state]
+		return [[[c for c in place[0]], place[1]] for place in self.state]
 
 	def __repr__(self):
 		return str(self.state)
 
-class Place(object):
-	"""A Place holds camels or a powerup."""
-
-	def __init__(self, camels):
-		"""Create a Place
-
-		camels -- list of camels in this place -- [bottom, ... , top]
-		powerup -- int {-1, 0, 1} of a powerup in this Place
-		"""
-		self.camels = camels
-		self.powerup = 0
-		
-	def add_camels(self, camels, under=False):
-		"""Add camels to this Place
-
-		camels -- list of camels to be added
-		under -- if True, place camels under instead
-		"""
-		for c in camels:
-			if c in self.camels:
-				assert False, "Camel already here!"
-
-		if under:
-			self.camels = camels + self.camels
-		else:
-			self.camels += camels
-
-	def remove_camels(self):
-		"""Remove all camels from this Place.
-
-		return -- list of camels (in same order) that were removed"""
-		r = self.camels
-		self.camels = []
-		return r
-
-	def remove_camel(self, camel_id):
-		"""Remove camel with camel_id from this Place, as well as all
-		camels that are on top of this camel.
-
-		return -- list of camels (in same order) that were removed"""
-		index = -1
-
-		for i in range(len(self.camels)):
-			if self.camels[i] == camel_id:
-				index = i
-				break
-
-		if index == -1:
-			assert False, "Camel not in this Place, cannot remove."
-
-		r = self.camels[index:]
-		self.camels = self.camels[:index]
-		return r
-
-	def has_camel_here(self, camel_id):
-		"""Returns true if camel_id is present at this Place."""
-		return camel_id in self.camels
-
-	def has_camels(self):
-		"""Returns true if there are any camels at this Place."""
-		return not self.camels == []
-
-	def place_powerup(self, powerup):
-		"""Places a powerup (-1 or 1) at this Place."""
-		if powerup not in [-1, 1]:
-			assert False, "powerup must be -1 or 1!"
-
-		if not self.camels == []:
-			assert False, "camels occupy this area; powerup can't be placed here!"
-
-		self.powerup = powerup
-
-	def remove_powerup(self):
-		"""Removes powerup from this Place."""
-		self.powerup = 0
-
-	def winning_camel(self):
-		"""Returns the top camel in this Place."""
-		return self.camels[len(self.camels) - 1]
-
-	def losing_camel(self):
-		"""Returns the lowest camel in this Place."""
-		return self.camels[0]
-
-	def fast_copy(self):
-		"""Returns a copy of this Place and does it FAST."""
-		if self.powerup != 0:
-			p = Place([])
-			p.powerup = self.powerup
-			return p
-		return Place([c for c in self.camels])
-
-	def __repr__(self):
-		return str(self.camels) + "[" + str(self.powerup) + "]"
-
-d = {0: [1, 2, 3], 1: [4], 2: [5]}
-b = Board(d)
+"""d = {0: [1, 2, 3], 1: [4], 2: [5]}
+b = Board(d)"""
